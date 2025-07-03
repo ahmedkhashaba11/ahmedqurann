251c36d0f335658fdecf886a84a7ebec25130951
DESKTOP-DOIJULJ\iShop <ahmedkhashaba492@gmail.com>
2025-07-04 02:59:47 +0300

    اول خلفيه للتطبيق بتدعم الابيض و الاسود

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,12 @@ plugins {
 }
 
 android {
+
+
     namespace = "com.example.qurann_project"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
